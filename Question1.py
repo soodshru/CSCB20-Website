7ea4b05e119ec693a2c23da1b48a60671c1b9ddf
soodshru <55465941+soodshru@users.noreply.github.com>
2020-03-03 13:18:02 -0500

    Added Question1.py

---
+++ Question1.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask (__name__)
+
+@app.route('/')
+def login():
+    return render_template('Login.html')
+
+# run the app when app.py is run
+if __name__ == '__main__':
+    app.run(debug='True')
