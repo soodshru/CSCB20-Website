9a0c4a95f86a5e9259ccfa2210024d37c5ab5426
soodshru <55465941+soodshru@users.noreply.github.com>
2020-03-08 15:55:22 -0400

    Working q1.py

--- Question1.py
+++ Question1.py
@@ -3,16 +3,18 @@ app = Flask (__name__)
 
 @app.route('/<name>')
 def  generateResponse(name):
-        if name.isupper() == 0 and name.isalpha():
-            WelcomeName = name.upper();
-        elif name.islower() == 0 and name.isalpha() :
+        if (name.isupper() and name.isalpha()):
             WelcomeName = name.lower();
+        elif (name.islower() and name.isalpha()) :
+            WelcomeName = name.upper();
+        elif (name.isalpha()):
+            WelcomeName = name;
         else :
-        	newName = ""
-        	for a in name :
-        		if a.isalpha() :
-        			newName += a
-        	WelcomeName = newName;
+            newName = ""
+            for a in name :
+                if a.isalpha() :
+                    newName += a
+            WelcomeName = newName;
         return "Welcome, "+WelcomeName+", to my CSCB20 website!";
         
 # run the app when app.py is run
