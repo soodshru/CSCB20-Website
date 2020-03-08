ad7ad309efbb4a2d4a971c484cb4a3171a8cb38f
soodshru <55465941+soodshru@users.noreply.github.com>
2020-03-08 14:44:12 -0400

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -3,8 +3,8 @@ app = Flask (__name__)
 
 @app.route('/')
 def login():
-    return render_template('Login.html')
+    return render_template('')
 
 # run the app when app.py is run
 if __name__ == '__main__':
-    app.run(debug= True)
+    app.run(debug = True)
