969643652b4ced1b88cad26dcc7fcaadd290f9db
soodshru <55465941+soodshru@users.noreply.github.com>
2020-03-08 15:15:58 -0400

    Made q1 parts 1 and 2 work

--- Question1.py
+++ Question1.py
@@ -3,7 +3,11 @@ app = Flask (__name__)
 
 @app.route('/<name>')
 def  generateResponse(name):
-    return "Welcome, "+name+", to my CSCB20 website!"
+        if name.isupper() == 0 :
+            WelcomeName = name.upper();
+        elif name.islower() == 0 :
+            WelcomeName = name.lower();
+        return "Welcome, "+WelcomeName+", to my CSCB20 website!";
 
 # run the app when app.py is run
 if __name__ == '__main__':
