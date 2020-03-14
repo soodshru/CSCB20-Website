af6a741fd4b026599678b552697af9137d687266
soodshru <55465941+soodshru@users.noreply.github.com>
2020-03-14 19:21:06 -0400

    Create Question1.py

---
+++ Question1.py
@@ -0,0 +1,25 @@
+from flask import Flask
+app = Flask (__name__)
+
+@app.route('/<name>')
+def  generateResponse(name):
+        if (name == ""):
+            return "Welcome to my CSCB20 website";
+        if (name.isupper() and name.isalpha()):
+            WelcomeName = name.lower();
+        elif (name.islower() and name.isalpha()) :
+            WelcomeName = name.upper();
+        elif (name.isalpha()):
+            WelcomeName = name;
+        else :
+            newName = ""
+            for a in name :
+                if a.isalpha() :
+                    newName += a
+            WelcomeName = newName;
+        return "Welcome, "+WelcomeName+", to my CSCB20 website!";
+
+
+# run the app when app.py is run
+if __name__ == '__main__':
+    app.run(debug = True)
